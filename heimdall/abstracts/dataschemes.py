dd6cdb28de9297dfd6edd819d17e3151669ef422
KanishkNavale <navalekanishk@gmail.com>
2023-12-23 13:58:08 +0100

    Solved: pydantic post_init issue

---
+++ heimdall/abstracts/dataschemes.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+from typing import TypeVar
+
+T = TypeVar("T", bound="BaseDataClass")
+
+class BaseDataClass(BaseModel):
+
+    class Config:
+        extra="forbid"
+        validate_assignment=True
+        arbitrary_types_allowed=True
+
+
+    @property
+    def as_dictionary(self) -> dict:
+        return self.model_dump()
+    
+    @property
+    def as_json(self) -> str:
+        return self.model_dump_json()
+    
+    @classmethod
+    def from_dictionary(cls: T, dictionary: dict) -> T:
+        return cls(**dictionary)
+    
+    def __post_init__(self, *kwargs) -> None:
+        pass
+
+    def model_post_init(self, *kwargs) -> None:
+        return self.__post_init__(*kwargs)
---
+++ heimdall/configurations/trainer.py
@@ -0,0 +1,31 @@
+import os
+
+from heimdall.abstracts.dataschemes import BaseDataClass
+
+class TrainerConfig(BaseDataClass):
+    precision: str
+    model_checkpoint_name: str
+    epochs: int
+    enable_logging: bool
+    tensorboard_path: bool
+    training_directory: str
+    enable_checkpointing: bool
+    model_path: str
+    logging_frequency: bool
+    validation_frequency: int
+
+    @staticmethod
+    def _create_dir(path: str) -> None:
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    def __post_init__(self) -> None:
+        self.training_directory = os.path.abspath(self.training_directory)
+        self.model_path = os.path.abspath(self.model_path)
+        self.tensorboard_path = os.path.abspath(self.tensorboard_path)
+
+        if self.precision not in ["medium", "high"]:
+            raise ValueError("Precision must be either {medium, high}")
+
+        self._create_dir(self.training_directory)
+        self._create_dir(self.tensorboard_path)
