035f461c7cc671fbfd55a2a38dc918fe6780c581
KanishkNavale <navalekanishk@gmail.com>
2024-01-24 14:20:07 +0100

    Added: Base ViT

--- heimdall/embeddings/patch_embedder.py
+++ heimdall/embeddings/patch_embedder.py
@@ -13,10 +13,12 @@ class PatchEmbedder(torch.nn.Module):
         out_channels: int,
         patch_size: int = 16,
         flat_dispatch: bool = False,
+        channel_last: bool = True,
     ):
         super(PatchEmbedder, self).__init__()
 
         self.flat_dispatch = flat_dispatch
+        self.channel_last = channel_last
 
         self.conv = torch.nn.Conv2d(
             in_channels=in_channels,
@@ -29,16 +31,30 @@ class PatchEmbedder(torch.nn.Module):
             self.position_embedder = PositionalEmbedder1D(
                 d_model=out_channels, max_len=2000
             )
+            self.class_token = torch.nn.Parameter(torch.randn(1, 1, out_channels))
         else:
             self.position_embedder = PositionalEmbedder2D(d_model=out_channels)
+            self.class_token = torch.nn.Parameter(torch.randn(1, 1, 1, out_channels))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.conv(x)
 
         if self.flat_dispatch:
-            x = x.flatten(-2, -1)
-            pos_embeddings = self.position_embedder(x.permute(0, 2, 1))
-            return x + pos_embeddings.permute(0, 2, 1)
+            flat_x = x.flatten(-2, -1).permute(0, 2, 1)
+            concat_cls_embeddings = torch.cat([self.class_token, flat_x], dim=1)
+            pos_embeddings = self.position_embedder(concat_cls_embeddings)
+            pos_cls_embeddings = concat_cls_embeddings + pos_embeddings
+            return (
+                pos_cls_embeddings
+                if self.channel_last
+                else pos_cls_embeddings.permute(0, 2, 1)
+            )
         else:
-            pos_embeddings = self.position_embedder(x.permute(0, 2, 3, 1))
-            return x + pos_embeddings.permute(0, 3, 1, 2)
+            concat_cls_embeddings = torch.cat([self.class_token, x], dim=2)
+            pos_embeddings = self.position_embedder(x)
+            pos_cls_embeddings = concat_cls_embeddings + pos_embeddings
+            return (
+                pos_cls_embeddings
+                if self.channel_last
+                else pos_cls_embeddings.permute(0, 3, 1, 2)
+            )
---
+++ heimdall/transformers/vision/base_vit.py
@@ -0,0 +1,31 @@
+import torch
+
+from heimdall.embeddings.patch_embedder import PatchEmbedder
+from heimdall.transformers.encoder_layer import Encoder
+
+
+class BaseViTFeature(torch.nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        patch_size: int,
+        encoder_head_size: int,
+        n_attention_heads: int,
+        n_encoder_layers: int,
+    ) -> None:
+        super(BaseViTFeature, self).__init__()
+
+        self.patch_embedder = PatchEmbedder(
+            in_channels, out_channels, patch_size, flat_dispatch=True
+        )
+        self.encoder = Encoder(
+            out_channels, encoder_head_size, n_attention_heads, n_encoder_layers
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.patch_embedder(x)
+        return self.encoder(x)
+
+    def compute_classification_logits(self, x: torch.Tensor) -> torch.Tensor:
+        return self.forward(x)[:, 0, :]
